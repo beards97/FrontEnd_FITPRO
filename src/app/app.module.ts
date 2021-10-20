9ae5ba4d88f2ca5455fba2bb819b92ec48111432
Manfred <mvalverde@ti-consultores.com>
2021-10-20 14:09:19 -0600

    Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListCustomersComponent } from './components/customers/list-customers/list-customers.component';
+import { AddCustomersComponent } from './components/customers/add-customers/add-customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    FooterComponent,
+    ListCustomersComponent,
+    AddCustomersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/customers.ts
@@ -0,0 +1,7 @@
+export class customers{
+    id?: number;
+    name?: string;
+    phone?: string;
+    email?: string;
+    notes?: string;
+}
